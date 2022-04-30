b54b99fefe941783a0c9b6e755a33796ca36c413
SecForForza <103151746+SecForForza@users.noreply.github.com>
2022-04-29 23:03:47 -0400

    Create Update.sql

---
+++ Update.sql
@@ -0,0 +1,7 @@
+update patient_treatment set treatment = 'Radiology' where doctor_id='DOC-101';
+update patient_treatment set treatment = 'Myopathy' where doctor_id='DOC-102';
+update patient_treatment set treatment = 'Diagnosis' where doctor_id='DOC-103';
+update patient_treatment set treatment = 'Removal' where doctor_id='DOC-104';
+update patient_treatment set treatment = 'Prognosis' where doctor_id='DOC-105';
+update patient_treatment set treatment = 'Biopsy' where doctor_id='DOC-106';
+update patient_treatment set treatment = 'Lapro' where doctor_id='DOC-107';
